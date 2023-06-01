9e7352c01432bd50bb3072a0654ae53a32adc1b3
Dmytro <dimores95@gmail.com>
2023-06-01 16:22:34 +0300

    lesson 6, intermediate result

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="m in messages" :key="m.text">{{ m.text }}</li>
-    </ul>
+  <div class="c-wrap">
+    <div class="c-chat">
+      <ul>
+        <li v-for="m in messages" :key="m.text">{{ m.text }}</li>
+      </ul>
+    </div>
+    <div class="c-form"></div>
   </div>
 </template>
 
@@ -20,3 +23,31 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.c-wrap {
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+}
+
+.c-chat {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 80px;
+  padding: 1rem;
+  overflow-y: auto;
+}
+
+.c-form {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 1rem;
+  height: 80px;
+  background: #212121;
+}
+</style>
